fdcb5da001039e1c1b2cefebe2aff7ef0f617050
wszemart <wszeborska@wp.pl>
2024-02-13 20:43:31 +0100

    Add Users app into the project

--- base/urls.py
+++ base/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from users.views import NewUserRegister
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
+    path("register/", NewUserRegister.as_view(), name="register"),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_URL)
---
+++ users/views.py
@@ -0,0 +1,50 @@
+from django.contrib import messages
+from django.contrib.auth import logout as auth_logout
+from django.shortcuts import redirect, render
+from django.views import View
+
+from .forms import CustomUserCreationForm
+
+
+class NewUserRegister(View):
+    template_name: str = "users/register.html"
+
+    def get(self, request) -> render:
+        form = CustomUserCreationForm()
+        return render(request, self.template_name, {"form": form})
+
+    def post(self, request):
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"Account's been created for {username}!")
+            return redirect("login")
+        return render(request, self.template_name, {"form": form})
+
+
+class LogoutUser(View):
+    def get(self, request) -> redirect:
+        auth_logout(request)
+
+        message: str = "You have been successfully logged out"
+        messages.success(request, message)
+
+        return redirect("login")
+
+
+# class UserProfile(LoginRequiredMixin, View):
+#     template_name: str = "users/profile.html"
+#
+#     def get(self, request):
+#         user_form = UserUpdateForm(instance=request.user)
+#         return render(request, self.template_name, {"user_form": user_form})
+#
+#     def post(self, request):
+#         user_form = UserUpdateForm(request.POST, instance=request.user)
+#         if user_form.is_valid():
+#             user_form.save()
+#             messages.success(request, "Your profile has been updated")
+#             return redirect("profile")
+#         messages.error(request, "There was an error updating your profile")
+#         return redirect("profile")
